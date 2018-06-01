13b282c925af7dbcf666a64f14f6767d7f673f48
elba7r <elba7r@gmail.com>
2018-06-01 09:31:34 +0200

    31-5-2018

---
+++ erpnext/stock/doctype/formulation_type/formulation_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Revaluesoft S.A.E and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Formulation Type', {
+	refresh: function(frm) {
+
+	}
+});
