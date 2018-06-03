e1d18bfee31782dcc9c99d4a12c78915d9a26b34
elba7r <elba7r@gmail.com>
2018-06-03 07:49:31 +0200

    3-6-2018

---
+++ erpnext/stock/doctype/scientific_type/scientific_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Revaluesoft S.A.E and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Scientific Type', {
+	refresh: function(frm) {
+
+	}
+});
